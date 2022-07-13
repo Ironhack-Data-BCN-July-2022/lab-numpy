306f9ae706bab71eef52472dab54ae8049a38577
samIronhack <samironhack@gmail.com>
2022-07-13 19:43:52 +0200

    updated

--- your-code/main.py
+++ your-code/main.py
@@ -7,7 +7,7 @@ print(np.__version__)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-a = np.random.random((7,3,4)) 
+a = np.random.random((2,3,5)) 
 
 #4. Print a.
 print(a)
@@ -15,51 +15,68 @@ print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5,2,3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+a.size == b.size
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+print(np.dot(a, b))
+#no becuase the array shapes are not equal
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+
+c = b.transpose(2,1,0)
 
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
+d = a+c
+
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
+print(d)
+#values are the same but d is a float and a is an integer everything in d == a+1
 
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e = a+c
 
 
 #13. Does e equal to a? Why or why not?
 
-
+d = a*c
+# yes, but the value types are different 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = d.max()
+d_mean = d.mean()
+d_min  = d.min()
 
+print(d_max)
+print(d_mean) 
+print(d_min)  
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty([2*3*5])
 
 
 
@@ -72,6 +89,35 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+x,y,z=-1,-1,-1
+for sub5 in d:
+        x+=1
+        for sub3 in sub5:
+                y+=1
+                for i in sub3:
+                        z+=1
+                        if i > d_min and i < d_mean:
+                            f[x,y,z] = 25
+                        elif i > d_mean and i < d_max:
+                            f[x,y,z] = 75
+                        elif i == d_mean:
+                            f[x,y,z] = 50
+                        elif i == d_min:
+                            f[x,y,z] = 0
+                        elif i == d_max:
+                            f[x,y,z] = 100
+                        else:
+                            f[x,y,z] = "#"
+                        if z == 4:
+                            z=-1
+                if y ==2:
+                        y=-1
+                        
+
+
+
+     
+
 
 
 
